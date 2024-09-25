51abfaffcb14a9070187d234b9e78cdaaf7c78a5
ravipal <palravi1093@gmail.com>
2024-09-25 15:16:35 +0530

    Initial commit for multiple projects

---
+++ src/test/java/com/example/SpringForm/SpringFormApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.SpringForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFormApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFormApplication.class, args);
+		System.out.println("Done");
+	}
+
+}
